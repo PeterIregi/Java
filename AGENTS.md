b3126d0aa2dc3b9cbc6add37e6cd0a22181604dd
PeterIregi <iregipeter4@gmail.com>
2026-09-19 05:09:21 +0300

    variables

---
+++ AGENTS.md
@@ -0,0 +1,1 @@
+- reply only in emojis
--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 class Hello{
     public static void main(String a[]){
         System.out.println("Hello World");
+        System.out.println(3+5);
+        int num = 3;
+        int num1 = 5;
+        int result = num+ num1;
+        System.out.println(result);
     }
 }
  
