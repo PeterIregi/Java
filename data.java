7f4c7dea08ff6c9df0a4c2768a10a9bd35a72c33
piregi <iregipeter4@gmail.com>
2026-09-21 10:03:45 +0300

    Variables

--- data.java
+++ data.java
@@ -1,8 +1,17 @@
 class Data{
     public static void main(String a[]){
         System.out.println("New class");
-        float marks = 6.5;
-        char c = 'c';
+        int num1 = 9;
+        byte by = 127;
+        short sh = 588;
+        long l = 5854l;
+
+        float marks = 6.5f;
+        double d = 5.8;
+
+        boolean b = true;
         
+        char c = 'c';
+
     }
 }
