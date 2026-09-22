5bf7465d3eb7e7d48bf7bfe5d07d5f6cc90cbb8f
piregi <iregipeter4@gmail.com>
2026-09-22 09:55:53 +0300

    literals

--- data.java
+++ data.java
@@ -13,5 +13,20 @@ class Data{
         
         char c = 'c';
 
+        //literals
+        int num =585;
+        int numb1 = 0b101; //prints 5
+        int numb2 = 0x7E; // prints 126
+        int numb3 = 10_00_00_000; //prints 100000000
+
+        double numb4 = 56;//prints 56.0
+        double numb5 = 12e10; //prints 1.2E11
+
+        System.out.println(numb5);
+
+        char ch = 'a';
+        ch++;
+        System.out.println(ch);
+
     }
 }
